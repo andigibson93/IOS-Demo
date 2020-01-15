cc1beea51e06b598a30c7cedb796083251f3ab99
Andi Gibson <andicsgibson@gmail.com>
2020-01-14 22:23:35 -0500

    commit IOS

--- MacDemo/ViewController.swift
+++ MacDemo/ViewController.swift
@@ -9,12 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var textLabel: UILabel!
+    
+    @IBOutlet weak var textField: UITextField!
+    var backgroundColor: UIColor!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        backgroundColor = view.backgroundColor
+    }
+    
+    @IBAction func didTapButton(_ sender: Any) {
+    print("Hello")
+        textLabel.textColor = UIColor.orange
     }
 
-
+    @IBAction func didTapViewButton(_ sender: Any) {
+        view.backgroundColor = UIColor.red
+    }
+    
+    @IBAction func didViewTextButton(_ sender: Any) {
+        textLabel.text = textField.text
+        textField.text = " "
+        view.endEditing(true)
+    
+    }
+    
+    @IBAction func onResetGesture(_ sender: Any) {
+    textLabel.text = "Hello"
+    view.backgroundColor = backgroundColor
+    }
+        
 }
 
